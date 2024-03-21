dd2521a9f121b03c9b19b3a0db1cd2b3f0413ad0
ly-rithy <lyrithy7722@gmail.com>
2024-03-21 19:23:22 +0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>Hello</h1>
+    <h1>Hello test</h1>
     <h1>Hello Kimsien</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
