a9b522ec1ae1bcdfbc76f5de707262a1b405d5bc
pidor-tha <tha.ratanakpidor@gmail.com>
2024-03-21 19:21:07 +0700

    Update HelloWorld.vue
    pidor commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h1>Hello Kimsien</h1>
+    <h1>Hello pidor</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
